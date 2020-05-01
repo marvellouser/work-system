2a38fbd22e425b4a3070709044b0feff20f0b88b
huayun_liutao <liutao1@szhcf.com.cn>
2020-05-01 22:03:46 +0800

    build-test

---
+++ build/precache-manifest.5f3ea9f35ed692e3f6191fa79baf0ba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed2051f1c4b5e61d3e72f89ecc4d928",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5feec08fb8529b341d0a",
+    "url": "./static/js/2.df8adb13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.df8adb13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9122d8b7a08fcdc794ea",
+    "url": "./static/js/main.8dd64593.chunk.js"
+  },
+  {
+    "revision": "157a4908a2ba99673c7f",
+    "url": "./static/js/runtime-main.4b789b93.js"
+  }
+]);

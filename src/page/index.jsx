63b886c949b275a883303f4d7371e611db64c185
huayun_liutao <liutao1@szhcf.com.cn>
2020-05-02 17:02:26 +0800

    test

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from '../components/Head'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Admin from './Admin'
 import About from './About'
 import Products from './Products'
@@ -8,20 +8,25 @@ import News from './News'
 import SellAndServer from './SellAndServer'
 import Human from './Human'
 import Contact from './Contact'
+import './style.scss'
 export default class Index extends React.PureComponent {
   render() {
     return (
       <div>
         <Head {...this.props} />
-        <Route path="/" exact component={Admin} />
-        <div className="container">
-          <Route path="/about" component={About} />
-          <Route path="/products" component={Products} />
-          <Route path="/news" component={News} />
-          <Route path="/sellandserver" component={SellAndServer} />
-          <Route path="/human" component={Human} />
-          <Route path="/contact" component={Contact} />
-        </div>
+        <Switch>
+          <Route path="/" exact component={Admin} />
+          <div className="container">
+            <div className="con">
+              <Route path="/about" component={About} />
+              <Route path="/products" component={Products} />
+              <Route path="/news" component={News} />
+              <Route path="/sellandserver" component={SellAndServer} />
+              <Route path="/human" component={Human} />
+              <Route path="/contact" component={Contact} />
+            </div>
+          </div>
+        </Switch>
       </div>
     )
   }
